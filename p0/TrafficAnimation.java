4f3e801df65986134844e8d360ea0d5cc28f4cd2
SilouanTheAthonite <bman022304@gmail.com>
2023-02-13 16:27:18 -0700

    TrafficAnimationFinal

--- p0/TrafficAnimation.java
+++ p0/TrafficAnimation.java
@@ -1,13 +1,20 @@
+/**
+ * 
+ * @author Bryce Younger
+ */
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Toolkit;
+import java.awt.Polygon;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 
 /**
  * CS 121 Project 0: Traffic Animation
@@ -42,7 +49,14 @@ public class TrafficAnimation extends JPanel
 	 */
 	private int stepSize = 10;
 
-	private final Color BACKGROUND_COLOR = Color.black;
+	
+	private final Color NIGHT_SKY = new Color (0, 0, 30);
+	Color nightDesert = new Color (54, 34, 12);
+	Color maroon = new Color (110, 0, 0);
+	Color carWindow = new Color (0, 37, 21);
+	Color cactiGreen = new Color (51, 102, 0);
+	Color moonYellow = new Color (254, 252, 215);
+	Color tan = new Color (255, 235, 205);
 
 	/* This method draws on the panel's Graphics context.
 	 * This is where the majority of your work will be.
@@ -57,21 +71,155 @@ public class TrafficAnimation extends JPanel
 		int height = getHeight(); // panel height
 
 		// Fill the graphics page with the background color.
-		g.setColor(BACKGROUND_COLOR);
+		g.setColor(NIGHT_SKY);
 		g.fillRect(0, 0, width, height);
-		
+
+		// Fills in the road
+		g.setColor(Color.black);
+		g.fillRect(0, (height / 4), width, (height / 2));
+
+		// Fills in the road markers
+		g.setColor(Color.yellow);
+		g.fillRect((width / 18), ((height / 5) * 2), (width / 9), (height / 20));
+		g.fillRect((width / 3), ((height / 5) * 2), (width / 9), (height / 20));
+		g.fillRect(((width / 18) * 11), ((height / 5) * 2), (width / 9), (height / 20));
+		g.fillRect(((width / 9) * 8), ((height / 5) * 2), (width / 9), (height / 20));
+
+		// Fills in desert floor
+		g.setColor(nightDesert);
+		g.fillRect(0, ((height / 4) * 3), width, (height / 4));
 
 		// Calculate the new xOffset position of the moving object.
 		xOffset  = (xOffset + stepSize) % width;
 
-		// This draws a green square. Replace it with your own object.
-		int squareW = height/5;
-		int squareH = squareW; 
-		int squareX = xOffset;
-		int squareY = height/2 - squareW/2;
+		// This draws a maroon chevy Impala circa. 2008
+		int tailLightW = height/20;
+		int tailLightH = tailLightW; 
+		int tailLightX = xOffset;
+		int tailLightY = height/2 - tailLightW/2;
+		
+		Polygon tailLight = new Polygon();
+		tailLight.addPoint(tailLightX, tailLightY);
+		tailLight.addPoint(tailLightX, (tailLightY + tailLightH));
+		tailLight.addPoint((tailLightX + tailLightW), (tailLightY + tailLightH));
 
-		g.setColor(Color.green);
-		g.fillRect(squareX, squareY, squareW, squareH);
+		// Draws the tail light
+		g.setColor(Color.red);
+		g.fillPolygon(tailLight);
+
+		g.setColor(Color.white);
+		g.fillRect(tailLightX, (tailLightY + tailLightH), tailLightW, (height / 50));
+
+		// Draws remainder of car rear
+		g.setColor(maroon);
+		g.fillArc((tailLightX - (tailLightW / 2)), ((tailLightY + tailLightH) + (height / 50)), tailLightW, (tailLightH * 2), 90, 180);
+		g.fillRect(tailLightX, ((tailLightY + tailLightH) + (height / 50)), (tailLightW * 3), (tailLightH * 2));
+
+		Polygon upperCarRear = new Polygon();
+		upperCarRear.addPoint(tailLightX, tailLightY);
+		upperCarRear.addPoint((tailLightX + tailLightW), (tailLightY + tailLightH));
+		upperCarRear.addPoint((tailLightX + tailLightW), ((tailLightY + tailLightH) + (height / 50)));
+		upperCarRear.addPoint((tailLightX + (tailLightW * 3)), ((tailLightY + tailLightH) + (height / 50)));
+		upperCarRear.addPoint((tailLightX + (tailLightW * 3)), tailLightY);
+		g.fillPolygon(upperCarRear);
+
+		// Draws mid-section of car
+		g.fillArc((tailLightX + (tailLightW * 3)), (tailLightY - tailLightH - (height / 50)), (tailLightW * 9), (tailLightH * 3), 0,180);
+
+		g.fillRect((tailLightX + (tailLightW * 3)), tailLightY, (tailLightW * 12), ((tailLightH * 3) + (height / 50)));
+
+		// Draws gray wheels of the car white white tires
+		g.setColor(Color.black);
+		g.fillArc((tailLightX + (tailLightW * 3)), ((tailLightY + (tailLightH * 2)) + (tailLightH / 50)), (tailLightW * 3), (tailLightH * 3), 0, 180);
+		g.fillArc((tailLightX + (tailLightW * 12)), ((tailLightY + (tailLightH * 2)) + (tailLightH / 50)), (tailLightW * 3), (tailLightH * 3), 0, 180);
+
+		g.setColor(Color.white);
+		g.fillOval((tailLightX + (tailLightW * 3) + (tailLightW / 2)), ((tailLightY + (tailLightH * 2)) + (tailLightH / 20)), (tailLightW * 2), (tailLightH * 2));
+		g.fillOval((tailLightX + (tailLightW * 12) + (tailLightW / 2)), ((tailLightY + (tailLightH * 2)) + (tailLightH / 20)), (tailLightW * 2), (tailLightH * 2));
+
+		g.setColor(Color.gray);
+		g.fillOval((tailLightX + (tailLightW * 3) + ((tailLightW / 5) * 4)), ((tailLightY + (tailLightH * 2)) + (tailLightH / 4)), ((tailLightW / 3) * 5), ((tailLightH / 3) * 5));
+		g.fillOval((tailLightX + (tailLightW * 12) + ((tailLightW / 5) * 4)), ((tailLightY + (tailLightH * 2)) + (tailLightH / 4)), ((tailLightW / 3) * 5), ((tailLightH / 3) * 5));
+
+		// Draws front end of car
+		g.setColor(maroon);
+		g.fillArc(tailLightX + (tailLightW * 14), (tailLightY - (tailLightH / 7)), (tailLightW * 3), tailLightH, 330, 210);
+		g.fillRect(tailLightX + (tailLightW * 15), tailLightY, ((tailLightW / 2) * 3), ((tailLightH * 3) + (height / 50)));
+
+		g.setColor(Color.white);
+		g.fillRect(tailLightX + (tailLightW * 15), (tailLightY + (tailLightH / 2)), ((tailLightW / 2) * 3), tailLightH);
+
+		g.setColor(Color.orange);
+		g.fillRect(tailLightX + (tailLightW * 15) + (tailLightW / 8), (tailLightY + ((tailLightH / 3) * 2)), ((tailLightW / 6) * 9), ((tailLightH / 6) * 5));
+
+		// Draws light from headlights
+		Polygon headLight = new Polygon();
+		headLight.addPoint(tailLightX + (tailLightW * 16), (tailLightY + (tailLightH / 2)));
+		headLight.addPoint(tailLightX + (tailLightW * 24), (tailLightY - tailLightH));
+		headLight.addPoint(tailLightX + (tailLightW * 24), (tailLightY + (tailLightH * 3)));
+		headLight.addPoint(tailLightX + (tailLightW * 16), (tailLightY + ((tailLightH / 2) * 3)));
+		g.fillPolygon(headLight);
+
+		// Draws doors and windows
+		g.setColor(carWindow);
+		g.fillArc((tailLightX + (tailLightW * 4)), (tailLightY - tailLightH), (tailLightW * 7), (tailLightH * 2), 0,180);
+
+		g.setColor(Color.black);
+		g.drawArc(tailLightX + (tailLightW * 6), ((tailLightY + tailLightH) + (height / 50) - (tailLightH / 2)), tailLightW, (tailLightH / 2), 0, 180);
+		g.drawLine(tailLightX + (tailLightW * 6), ((tailLightY + tailLightH) + (height / 50) - (tailLightH / 4)), tailLightX + (tailLightW * 7), ((tailLightY + tailLightH) + (height / 50) - (tailLightH / 4)));
+		g.drawArc(tailLightX + (tailLightW * 8), ((tailLightY + tailLightH) + (height / 50) - (tailLightH / 2)), tailLightW, (tailLightH / 2), 0, 180);
+		g.drawLine(tailLightX + (tailLightW * 8), ((tailLightY + tailLightH) + (height / 50) - (tailLightH / 4)), tailLightX + (tailLightW * 9), ((tailLightY + tailLightH) + (height / 50) - (tailLightH / 4)));
+
+		g.drawLine(tailLightX + (tailLightW * 4), tailLightY, tailLightX + ((tailLightW / 2) * 13), tailLightY + (tailLightH * 3));
+		g.drawLine(tailLightX + ((tailLightW / 2) * 13), tailLightY + (tailLightH * 3), tailLightX + (tailLightW * 10), tailLightY + (tailLightH * 3));
+		g.drawLine(tailLightX + (tailLightW * 10), tailLightY + (tailLightH * 3), tailLightX + (tailLightW * 11), tailLightY);
+
+		// Draws cacti
+		g.setColor(cactiGreen);
+		g.fillRect((width / 5), ((height / 16) * 11), (width / 20), ((height / 16) * 5));
+		g.fillRect((width / 6), ((height / 16) * 13), (width / 20), (height / 16));
+		g.fillRect((width / 4), ((height / 16) * 13), (width / 20), (height / 16));
+		g.fillRect((width / 7), ((height / 16) * 11), (width / 30), ((height / 16) * 3));
+		g.fillRect(((width / 7) * 2), ((height / 16) * 11), (width / 30), ((height / 16) * 3));
+		
+		g.fillRect(((width / 5) * 4), ((height / 16) * 11), (width / 20), ((height / 16) * 5));
+		g.fillRect(((width / 6) * 5), ((height / 16) * 13), (width / 20), (height / 16));
+		g.fillRect(((width / 4) * 3), ((height / 16) * 13), (width / 20), (height / 16));
+		g.fillRect(((width / 8) * 7), ((height / 16) * 11), (width / 30), ((height / 16) * 3));
+		g.fillRect(((width / 4) * 3), ((height / 16) * 11), (width / 30), ((height / 16) * 3));
+
+		// Draws moon and stars
+		g.setColor(moonYellow);
+		g.fillOval((width / 6), (height / 30), (width / 10), (height / 8));
+
+		g.setColor(Color.white);
+		g.fillOval((width / 3), (height / 20), (width / 100), (height / 80));
+		g.fillOval((width / 7), (height / 30), (width / 100), (height / 80));
+		g.fillOval((width / 8), (height / 10), (width / 100), (height / 80));
+		g.fillOval((width / 2), (height / 8), (width / 100), (height / 80));
+		g.fillOval(((width / 4) * 3), (height / 9), (width / 100), (height / 80));
+		g.fillOval(((width / 3) * 2), (height / 12), (width / 100), (height / 80));
+		g.fillOval(((width / 6) * 5), (height / 6), (width / 100), (height / 80));
+		g.fillOval(((width / 12) * 11), (height / 14), (width / 100), (height / 80));
+		g.fillOval(((width / 5) * 2), (height / 5), (width / 100), (height / 80));
+		g.fillOval((width / 15), (height / 5), (width / 100), (height / 80));
+
+		// Creates bi-stander in between cacti
+		g.setColor(tan);
+		g.fillOval((width / 2), ((height / 5) * 4), (width / 20), (height / 16));
+		
+		g.setColor(maroon);
+		g.drawLine(((width / 2) + (width / 40)), ((height / 8) * 7 + (height / 16)), ((width / 2) + (width / 40)), ((height / 48) * 43));
+		g.setColor(Color.blue);
+		g.drawLine(((width / 2) + (width / 40)), ((height / 8) * 7 + (height / 16)), ((width / 2) + (width / 20)), ((height / 100) * 99));
+		g.drawLine(((width / 2) + (width / 40)), ((height / 8) * 7 + (height / 16)), (width / 2), ((height / 100) * 99));
+		g.setColor(tan);
+		g.drawLine(((width / 2) + (width / 40)), ((height / 8) * 7 + (height / 32)), ((width / 2) + (width / 17)), ((height / 25) * 22));
+		g.drawLine(((width / 2) + (width / 40)), ((height / 8) * 7 + (height / 32)), ((width / 2) - (width / 100)), ((height / 25) * 22));
+
+		// Draws Dostoevsky quote
+		g.setColor(Color.white);
+		g.drawString("\"The darker the night, the brighter the stars, the deeper the grief, the closer is God!\" - Fyodor Dostoevsky", (width / 7), (height / 3));
 
 		// TODO: Use width, height, and xOffset to draw your scalable objects
 		// at their new positions on the screen
