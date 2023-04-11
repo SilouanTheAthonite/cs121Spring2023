b0d60209b864faf2fae48a7147e8a8a201c15714
SilouanTheAthonite <bman022304@gmail.com>
2023-04-11 14:48:57 -0600

    Beginnging of Class 4/11

--- p4/TicTacToe/TicTacToeGame.java
+++ p4/TicTacToe/TicTacToeGame.java
@@ -1,8 +1,52 @@
+import java.awt.Point;
+
 /**
- * 
+ * Impements the TicTacToe interface by defining its methods and allowing for the user to play a game of TicTacToe with the computer
  * @semester Spring 2023
  * @author Bryce Younger
  */
 public class TicTacToeGame implements TicTacToe{
+
+    private 
+
+    /**public TicTacToeGame(){
+        throw new UnsupportedOperationException("Unimplemented method");
+    }*/
+    
+    @Override
+    public void newGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newGame'");
+    }
+
+    @Override
+    public boolean choose(TicTacToe.BoardChoice player, int row, int col) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'choose'");
+    }
+
+    @Override
+    public boolean gameOver() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gameOver'");
+    }
+
+    @Override
+    public TicTacToe.GameState getGameState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameState'");
+    }
+
+    @Override
+    public TicTacToe.BoardChoice[][] getGameGrid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameGrid'");
+    }
+
+    @Override
+    public Point[] getMoves() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMoves'");
+    }
     
 }
